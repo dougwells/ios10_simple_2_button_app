783b5bb5d4a369e26b3cad42fe61df6665116bf9
dougwells <dougwells@gmail.com>
2016-11-02 06:27:11 -0600

    iOS. Simple2Button.Add functionality to green & blue button

--- simple2ButtonApp/ViewController.swift
+++ simple2ButtonApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func blueBtnTapped(_ sender: Any) {
+        colorView.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func greenBtnTapped(_ sender: Any) {
+        colorView.backgroundColor = UIColor.green
+    }
+    
+    
 }
 
